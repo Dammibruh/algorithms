ef8b4d79a41699f55e71dfefb8c49995ea420eed
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-30 00:29:52 +0100

    Update selectionsort.js

--- sort/selectionsort.js
+++ sort/selectionsort.js
@@ -1,7 +1,7 @@
 function selectsort(n){
     for(var i = 0; i < n.length;i++){
         for(var j = n.length; j > i; j--){
-            if(n[i] < n[j]){
+            if(n[j] < n[i]){
                 [n[j], n[i]] = [n[i], n[j]]
             }
         }
