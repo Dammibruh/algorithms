fb6e49e54330f6a1511438aaaec469afa8b738d0
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-30 00:25:06 +0100

    Create selectionsort.js

---
+++ sort/selectionsort.js
@@ -0,0 +1,11 @@
+function selectsort(n){
+    for(var i = 0; i < n.length;i++){
+        for(var j = 0; j < i && j < n.length-1; j++){
+            if(n[i] < n[j]){
+                [n[j], n[i]] = [n[i], n[j]]
+            }
+        }
+    }
+    return n
+}
+var b = [5,6,3,2,5,6,3]
