67ff1a2799fab01c6a36391c99476b2ee5b22042
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-12-10 19:20:23 +0100

    Update linked_list.cpp

--- data_structures/linked_list.cpp
+++ data_structures/linked_list.cpp
@@ -1,32 +1,80 @@
+#include <memory>
 #include <iostream>
 
-
-class Node{
-public:
-    int data;
-    Node *next;
+template<typename NodeType>
+class List {
+    std::shared_ptr<List> next;
+    NodeType data;
+    template<size_t N>
+    void _add_nodes(const NodeType (&node)[N]){
+        auto current = this;
+        for(int i = 0; i <= N-1;++i){
+            current->add_node(node[i]);
+            current = current->getNext().get();
+        }
+    }
+    auto _get_last_node(){
+        auto current = this;
+        while(current->getNext() != nullptr){
+            current = current->getNext().get();
+        }
+        return current;
+    }
+    public:
+    template<size_t N>
+    List(const NodeType (&node)[N])
+    {
+            this->_add_nodes(node);
+    }
+    List(const NodeType& data)
+        : data(data) {}
+    void add_node(const NodeType& data){
+        auto nextNode = std::make_shared<List<NodeType>>(data);
+        this->_get_last_node()->setNext(nextNode);
+    }
+    void add_node_first(const NodeType& data){
+        auto nnode = std::make_shared<List<NodeType>>(this->data);
+        nnode.get()->setNext(this->next);
+        this->data = data;
+    }
+    auto getNext(){
+        return this->next;
+    }
+    void setNext(const std::shared_ptr<List<NodeType>>& node){
+        this->next = node;
+    }
+    void pop_last(){
+        auto nextn = this->next.get();
+        while(nextn != nullptr){
+            nextn = nextn->getNext().get();
+        }
+        nextn = nullptr;
+    }
+    void pop_first(){
+        this->next = this->next.get()->getNext();
+        this->data = this->next.get()->getData();
+    }
+    size_t getSize(){
+        size_t size = 0;
+        auto current = this;
+        while(current->getNext() != nullptr){
+            current = current->getNext().get();
+            size++;
+        }
+        return size;
+    }
+    const NodeType& operator[](size_t index){
+        size_t count = 0;
+        auto current = this;
+        while(current->getNext() != nullptr){
+            current = current->getNext().get();
+            count++;
+            if(count-1 == index){
+                return current->getData();
+            }
+        }
+    }
+    NodeType getData(){
+        return this->data;
+    }
 };
-void getTotalNodes(Node *node){
-	int count = 1;
-	Node *cur = node;
-	while(cur->next){
-		cur = cur->next;
-		count++;
-	}
-	std::cout << "Total connected nodes is " << count;
-}
-int main(){
-   Node node;
-   Node node2;
-   Node node3;
-   Node node4;
-   node.data = 1;
-   node2.data = 2;
-   node3.data = 3;
-   node4.data = 4;
-   node.next = &node2;
-   node2.next = &node3;
-   node3.next = &node4;
-   getTotalNodes(&node);
-   //should print 4
-}
