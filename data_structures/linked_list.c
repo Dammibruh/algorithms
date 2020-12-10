5d1f2c0e2e045d59910165b28c40450cd5114300
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-12-10 12:28:44 +0100

    Create linked_list.c

---
+++ data_structures/linked_list.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+typedef struct Node {
+    struct Node* next;
+    int data;
+};
+struct Node* head;
+
+struct Node* last_node(){
+    struct Node* cur;
+    cur = head;
+    while(cur->next != NULL){
+        cur = cur->next;
+    }
+    return cur;
+}
+int node_size(){
+    struct Node* _current;
+    _current = head;
+    int count = 0;
+    while(_current != NULL){
+        _current = _current->next;
+        count++;
+    }
+    return count;
+}
+void add_node(int data){
+    struct Node* _next = (struct Node*)malloc(sizeof(struct Node));;
+    _next->data = data;
+    if(head == NULL){
+        head = _next;
+        head->next = NULL;
+    } else {
+        struct Node* l;
+        l = last_node();
+        l->next = _next;
+    }
+}
+void delete_node(){
+    struct Node *cur, *n;
+    cur = head;
+    while(cur->next != NULL){
+        n = cur;
+        cur = cur->next;
+    }
+    n->next = NULL;
+}
